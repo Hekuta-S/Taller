b12a168bf1f82c86d91e8f35702b0f61c77c081a
Hekuta <cabeuno@hotmail.com>
2021-08-26 14:06:02 -0500

    ejercicio 3

---
+++ Ejercicio3/Cliente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio3
+{
+    class Cliente
+    {
+        int numeroCuenta;
+        int saldoApertura;
+        string nombre;
+
+        public Cliente(string nombre, int numeroCuenta, int saldoApertura)
+        {
+            this.nombre = nombre;
+            this.numeroCuenta = numeroCuenta;
+            this.saldoApertura = saldoApertura;
+        }
+
+        public Cliente()
+        {
+           
+        }
+        
+        public void Consignar(int valorConsignacion)
+        {
+            this.saldoApertura = this.saldoApertura + valorConsignacion;
+        }
+
+        public void Retirar(int valorRetiro)
+        {
+            this.saldoApertura = this.saldoApertura - valorRetiro;
+        }
+
+        public string MostrarSaldo()
+        {
+            return ("Su saldo es: " + this.saldoApertura);
+        }
+
+
+
+    }
+}
---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre, respuesta;
+            int numeroCuenta, saldoApertura, monto=0;
+            Console.WriteLine("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su número de cuenta: ");
+            numeroCuenta = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite su saldo de apertura: ");
+            saldoApertura = int.Parse(Console.ReadLine());
+            Console.WriteLine("Quiere Consignar(C) o Retirar(R) su dinero? ");
+            respuesta = Console.ReadLine();
+
+            Cliente cliente = new Cliente(nombre, numeroCuenta, saldoApertura);
+            
+            if (respuesta == "C")
+            {
+                Console.WriteLine("Que valor va a consignar? ");
+                monto = int.Parse(Console.ReadLine());
+                cliente.Consignar(monto);
+            }
+            else if (respuesta == "R")
+            {
+                Console.WriteLine("Que valor va a retirar? ");
+                monto = int.Parse(Console.ReadLine());
+                cliente.Retirar(monto);
+            }
+            
+            Console.WriteLine(cliente.MostrarSaldo());
+        }
+       
+    }
+}
