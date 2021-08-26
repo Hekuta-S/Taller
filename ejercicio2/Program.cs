96245114d7211a7092ca71cfa2431eb035ab14c2
Hekuta <cabeuno@hotmail.com>
2021-08-26 01:43:52 -0500

    ejercicio 2

---
+++ ejercicio2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            string sexo;
+            double edad, pulsaciones = 0;
+            Console.WriteLine("su sexo es femenino(F) o masculino(M)?");
+            sexo = Console.ReadLine();
+            Console.WriteLine("Cual es su edad? ");
+            edad = double.Parse(Console.ReadLine()); 
+
+            if (sexo == "F")
+            {
+                pulsaciones = (220 - edad) / 10;
+
+            }
+            else if (sexo == "M") {
+                pulsaciones = (210 - edad) / 10;
+            }
+              
+            Console.WriteLine("El número de sus pulsaciones es: " + pulsaciones); 
+        }
+    }
+}
